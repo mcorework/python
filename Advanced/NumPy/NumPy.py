19909f081a40d328c57753c80eec89d4bd64b8da
Manas Samantray <mcorework@gmail.com>
2025-10-17 17:57:06 -0400

    Formatted the NumPy file.

--- Advanced/NumPy/NumPy.py
+++ Advanced/NumPy/NumPy.py
@@ -1,156 +1,178 @@
-# Import libraries for Num Py
+"""
+numpy_basics_demo.py
+---------------------
+Comprehensive demonstration of NumPy fundamentals:
+- Array creation
+- Indexing & slicing
+- Mathematical operations
+- Linear algebra
+- Statistics
+- Reshaping and stacking
+- File I/O
+- Boolean masking and advanced indexing
+
+References:
+    https://www.youtube.com/watch?v=QUT1VHiLmmI&t=1041s
+    https://github.com/KeithGalli/NumPy/blob/master/NumPy%20Tutorial.ipynb
+    https://numpy.org/doc/stable/reference/routines.math.html
+    https://numpy.org/doc/stable/reference/routines.linalg.html
+"""
+
+# Import library
 import numpy as np
 
-#######################################     BASICS     ########################################
-a = np.array([1,2,3], dtype='int32')
-print(a)
-b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0],[4.0,1.0,3.0]])
-print(b)
+# =============================================================================================
+#                                         BASICS
+# =============================================================================================
 
+# 1D and 2D arrays
+a = np.array([1, 2, 3], dtype='int32')
+print("Array a:", a)
 
-# https://www.youtube.com/watch?v=QUT1VHiLmmI&t=1041s (Youtube Video)
-# https://github.com/KeithGalli/NumPy/blob/master/NumPy%20Tutorial.ipynb (Github for NumPy)
-# https://numpy.org/doc/stable/reference/routines.math.html  (Math Library)
-# https://numpy.org/doc/stable/reference/routines.linalg.html  (Linear Algebra Library)
+b = np.array([
+    [9.0, 8.0, 7.0],
+    [6.0, 5.0, 4.0],
+    [4.0, 1.0, 3.0]
+])
+print("Array b:\n", b)
 
-# How to get the dimension of the Array
-print (b.ndim)
+# Basic properties
+print("Dimension of b:", b.ndim)
+print("Shape of b:", b.shape)
+print("Data type of a:", a.dtype)
+print("Memory size of a:", a.nbytes)
+print("Item size of b:", b.itemsize)
 
-# Get Shape
-print (b.shape)
+# =============================================================================================
+#                        ACCESSING / CHANGING ELEMENTS, ROWS, COLUMNS
+# =============================================================================================
 
-# Get data type of the object
-print (a.dtype)
+a = np.array([
+    [1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14]
+])
+print("\nArray a:\n", a)
 
-# Get Size
-print (a.nbytes)
-print (b.itemsize)
+# Accessing elements
+print("Element [0,5]:", a[0, 5])
+print("Row 1 onwards:\n", a[1:])
+print("Column 2:\n", a[:, 2])
 
+# Slicing with step size
+print("Fancy slice [0,1:4:2]:", a[0, 1:4:2])
 
-################# ACCESSING/CHANGING SPECIFIC ELEMENTS, ROWS, COLUMNS #######################
-a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
-print(a)
+# Modifying values
+a[1, 5] = 20
+a[:, 2] = [13, 17]
+print("Modified array a:\n", a)
 
-# Get a specific element [r, c]
-print(a[0, 5])
+# 3D array example
+b = np.array([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]]
+])
+b[:, 1, :] = [[11, 12], [13, 15]]
+print("\n3D array b:\n", b)
+print("Element b[0,1,0]:", b[0, 1, 0])
 
-# Get a specific row 
-print(a[1 :])
+# =============================================================================================
+#                        INITIALIZING DIFFERENT TYPES OF ARRAYS
+# =============================================================================================
 
-# Get a specific column
-print(a[:, 2])
+print("\n--- Array Initialization Examples ---")
+print("Zeros:\n", np.zeros((2, 3)))
+print("Ones:\n", np.ones((4, 2, 2), dtype='int32'))
+print("Full matrix (99):\n", np.full((2, 2), 99))
+print("Full_like (shape of a, fill with 4):\n", np.full_like(a, 4))
+print("Random decimal numbers:\n", np.random.rand(4, 2))
 
-# Getting a little more fancy [startindex:endindex:stepsize]
-print (a[0, 1:4:2])
-a[1,5] = 20
-a[:,2] = [13,17]
-print(a)
-
-# 3D example
-b = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
-b[:,1,:] = [[11,12], [13,15]]
-print(b)
-print(b[0,1,0])
-
-################# INITIALIZING DIFFERENT TYPES OF ARRAYS #######################
-# All 0s matrix
-np.zeros((2,3))
-# All 1s matrix
-np.ones((4,2,2), dtype='int32')
-# Any other number
-np.full((2,2), 99)
-# Any other number (full_like)
-np.full_like(a, 4)
-# Random decimal numbers
-np.random.rand(4,2)
-
-# Initialize and Draw a 5X5 array 
-arr1 = np.ones((5,5))
-arr2 = np.zeros((3,3), dtype='int16')
+# Creating a 5x5 matrix with a hollow center
+arr1 = np.ones((5, 5))
+arr2 = np.zeros((3, 3), dtype='int16')
 arr1[1:4, 1:4] = arr2
-arr1[2,2] = 9
-print(arr1)
+arr1[2, 2] = 9
+print("\n5x5 patterned array:\n", arr1)
 
-# Initialize and Draw a 5X5 array 
-arr1 = np.ones((5,5))
-arr2 = np.zeros((3,3), dtype='int16')
-arr1[1:4, 1:4] = arr2
-arr1[2,2] = 9
-print(arr1)
-
-# Copying Array
-a = np.array([20,30,40])
-b = a.copy() # Just copies the content not the reference
+# Copying arrays (deep copy)
+a = np.array([20, 30, 40])
+b = a.copy()
 a[1] = 90
-print(b)
+print("\nOriginal a modified:", a)
+print("Copied b remains:", b)
 
+# =============================================================================================
+#                                         MATHEMATICS
+# =============================================================================================
 
-################# MATHEMATICS #######################
-a = np.array([1,2,3,4])
-a+2
-a-2
-a+=2
-a/2
-a*2
-b = np.array([1,0,1,0])
-a + b
-# Take the sin
-np.cos(a)
+a = np.array([1, 2, 3, 4])
+b = np.array([1, 0, 1, 0])
 
-################# LINEAR ALGEBRA #######################
-a = np.full((6,8),1)
-a = np.ones((2,3))
-b = np.full((3,2), 8) 
-c = np.matmul(a,b)
-print (c)
+print("\n--- Math Operations ---")
+print("a + 2:", a + 2)
+print("a - 2:", a - 2)
+print("a * 2:", a * 2)
+print("a / 2:", a / 2)
+print("a + b:", a + b)
+print("cos(a):", np.cos(a))
 
-# Find the determinant
-c = np.identity(3)
-np.linalg.det(c)
+# =============================================================================================
+#                                      LINEAR ALGEBRA
+# =============================================================================================
+
+print("\n--- Linear Algebra ---")
+a = np.ones((2, 3))
+b = np.full((3, 2), 8)
+c = np.matmul(a, b)
+print("Matrix multiplication result:\n", c)
 
 # Determinant
-# Trace
-# Singular Vector Decomposition
-# Eigenvalues
-# Matrix Norm
-# Inverse
-# Etc...
+c = np.identity(3)
+print("Determinant of identity matrix:", np.linalg.det(c))
 
+# =============================================================================================
+#                                        STATISTICS
+# =============================================================================================
 
-################# STATISTICS #######################
-stats = np.array([[1,2,3000],[4,134,6]])
-print(np.max(stats, axis=1))
-print(np.sum(stats))
+print("\n--- Statistics ---")
+stats = np.array([[1, 2, 3000], [4, 134, 6]])
+print("Max along axis=1:", np.max(stats, axis=1))
+print("Sum of all elements:", np.sum(stats))
 
+# =============================================================================================
+#                                    REORGANIZING ARRAYS
+# =============================================================================================
 
+before = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+after = before.reshape((4, 2))
+print("\nReshaped array:\n", after)
 
-################# REORGANIZING ARRAYS #######################
-before = np.array([[1,2,3,4],[5,6,7,8]])
-after =before.reshape((4,2))
-print(after)
+# Vertical stack
+v1 = np.array([1, 2, 3, 4])
+v2 = np.array([5, 6, 7, 8])
+print("Vertical stack:\n", np.vstack([v1, v2, v1, v2]))
 
-# Vertically stacking vectors
-v1 = np.array([1,2,3,4])
-v2 = np.array([5,6,7,8])
-print(np.vstack([v1,v2,v1,v2]))
+# Horizontal stack
+h1 = np.ones((2, 4))
+h2 = np.zeros((2, 2))
+print("Horizontal stack:\n", np.hstack((h1, h2)))
 
-# Horizontal  stack
-h1 = np.ones((2,4))
-h2 = np.zeros((2,2))
-np.hstack((h1,h2))
+# =============================================================================================
+#                              MISCELLANEOUS / FILE OPERATIONS
+# =============================================================================================
 
-################# MISCELLANEOUS #######################
-# Load Data from File
+# Load data from file
 filedata = np.genfromtxt('data.txt', delimiter=',')
 filedata = filedata.astype('int32').copy()
-print(filedata)
-print("printing the axis 0 ------ ",np.any(filedata > 90, axis=0))
-print("printing the axis >50 & <90 ------ ",((filedata > 50) & (filedata <90)))
+print("\nLoaded data:\n", filedata)
 
-# Boolean Masking and Advanced Indexing
-print(filedata > 50)
-print(filedata[filedata > 50])
+# Boolean operations
+print("Any > 90 along axis 0:", np.any(filedata > 90, axis=0))
+print("Values > 50 and < 90:\n", (filedata > 50) & (filedata < 90))
 
-# You can index with a list in NumPy
-a = np.array([1,2,3,4,5,6,7,8,9])
-print(a[[3,5,7]])
+# Boolean masking
+print("Mask (filedata > 50):\n", filedata > 50)
+print("Filtered values (filedata > 50):", filedata[filedata > 50])
+
+# Advanced indexing
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print("Indexed elements [3,5,7]:", a[[3, 5, 7]])
