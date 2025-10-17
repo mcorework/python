c80820d5d5b8351f06ebf36d0161cfae48522a1f
Manas Samantray <mcorework@gmail.com>
2025-10-17 17:53:08 -0400

    Finished first part of NumPy.

--- Advanced/NumPy/NumPy.py
+++ Advanced/NumPy/NumPy.py
@@ -8,7 +8,11 @@ b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0],[4.0,1.0,3.0]])
 print(b)
 
 
-# https://github.com/KeithGalli/NumPy/blob/master/NumPy%20Tutorial.ipynb
+# https://www.youtube.com/watch?v=QUT1VHiLmmI&t=1041s (Youtube Video)
+# https://github.com/KeithGalli/NumPy/blob/master/NumPy%20Tutorial.ipynb (Github for NumPy)
+# https://numpy.org/doc/stable/reference/routines.math.html  (Math Library)
+# https://numpy.org/doc/stable/reference/routines.linalg.html  (Linear Algebra Library)
+
 # How to get the dimension of the Array
 print (b.ndim)
 
@@ -92,3 +96,61 @@ b = np.array([1,0,1,0])
 a + b
 # Take the sin
 np.cos(a)
+
+################# LINEAR ALGEBRA #######################
+a = np.full((6,8),1)
+a = np.ones((2,3))
+b = np.full((3,2), 8) 
+c = np.matmul(a,b)
+print (c)
+
+# Find the determinant
+c = np.identity(3)
+np.linalg.det(c)
+
+# Determinant
+# Trace
+# Singular Vector Decomposition
+# Eigenvalues
+# Matrix Norm
+# Inverse
+# Etc...
+
+
+################# STATISTICS #######################
+stats = np.array([[1,2,3000],[4,134,6]])
+print(np.max(stats, axis=1))
+print(np.sum(stats))
+
+
+
+################# REORGANIZING ARRAYS #######################
+before = np.array([[1,2,3,4],[5,6,7,8]])
+after =before.reshape((4,2))
+print(after)
+
+# Vertically stacking vectors
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+print(np.vstack([v1,v2,v1,v2]))
+
+# Horizontal  stack
+h1 = np.ones((2,4))
+h2 = np.zeros((2,2))
+np.hstack((h1,h2))
+
+################# MISCELLANEOUS #######################
+# Load Data from File
+filedata = np.genfromtxt('data.txt', delimiter=',')
+filedata = filedata.astype('int32').copy()
+print(filedata)
+print("printing the axis 0 ------ ",np.any(filedata > 90, axis=0))
+print("printing the axis >50 & <90 ------ ",((filedata > 50) & (filedata <90)))
+
+# Boolean Masking and Advanced Indexing
+print(filedata > 50)
+print(filedata[filedata > 50])
+
+# You can index with a list in NumPy
+a = np.array([1,2,3,4,5,6,7,8,9])
+print(a[[3,5,7]])
